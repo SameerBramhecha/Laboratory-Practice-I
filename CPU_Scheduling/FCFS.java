39d40a9b400e1b55308984585204dae9187005c5
Sameer Manoj Bramhecha <bramhechas89@gmail.com>
2022-11-17 19:39:59 +0530

    updated CPU_Scheduling

--- CPU_Scheduling/FCFS.java
+++ CPU_Scheduling/FCFS.java
@@ -1,46 +1,94 @@
 import java.util.*;
 
+class Process {
+	int pid;
+	int at;
+	int bt;
+	int wt;
+	int tat;
+
+	Process(int pid, int at, int bt) {
+		this.pid = pid;
+		this.at = at;
+		this.bt = bt;
+	}
+
+	public int arrivtime() {
+		return at;
+	}
+}
+
 public class FCFS {
+
+	static Process proc[];
+	static int p;
+	static double avgwt = 0;
+	static double avgtat = 0;
+
+	public static void input(Scanner sc) {
+		System.out.println("Enter Number of Processes");
+		p = sc.nextInt();
+		proc = new Process[p];
+		for (int i = 0; i < p; i++) {
+			System.out.println("Enter ProcessNo., Arrival Time and Burst Time: ");
+			int pid = sc.nextInt();
+			int at = sc.nextInt();
+			int bt = sc.nextInt();
+			proc[i] = new Process(pid, at, bt);
+		}
+
+	}
+
+	public static void fcfs() {
+		Arrays.sort(proc, new Comparator<Process>() {
+			@Override
+			public int compare(Process a, Process b) {
+				return a.arrivtime() - b.arrivtime();
+			}
+		});
+
+		proc[0].wt = 0;
+		for (int i = 1; i < p; i++) {
+			proc[i].wt = proc[i - 1].bt + proc[i - 1].wt;
+		}
+
+		for (int i = 0; i < p; i++) {
+			proc[i].tat = proc[i].bt + proc[i].wt;
+		}
+
+		for (Process i : proc) {
+			avgwt += i.wt;
+		}
+		avgwt /= p;
+		for (Process i : proc) {
+			avgtat += i.tat;
+		}
+		avgtat /= p;
+	}
+
+	public static void print() {
+		System.out.println("ProcessNo.\tBurstTime\tArrivalTime\tWaitingTime\tTurnAroundTime");
+		for (Process i : proc) {
+			System.out.println(i.pid + "\t\t" + i.bt + "\t\t" + i.at + "\t\t" + i.wt + "\t\t" + i.tat);
+		}
+
+		System.out.println("Average Waiting Time: " + avgwt);
+		System.out.println("Average Turn Around Time: " + avgtat);
+	}
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter Number of Processes");
-		int n = sc.nextInt();
-		int pn[] = new int[n];
-		int bt[] = new int[n];
-		for (int i = 0; i < n; i++) {
-			System.out.println("Enter process number: ");
-			pn[i] = sc.nextInt();
-			System.out.println("Enter Burst Time: ");
-			bt[i] = sc.nextInt();
+
+		input(sc);
+		fcfs();
+		System.out.println("Input Sorted According to ArrivalTime: ");
+		System.out.println("ProcessNo.\tBurstTime\tArrivalTime");
+		for (int i = 0; i < p; i++) {
+			System.out.println(proc[i].pid + "\t\t" + proc[i].bt + "\t\t" + proc[i].at);
 		}
 
-		int wt[] = new int[n];
-		int tat[] = new int[n];
-		wt[0] = 0;
-		for (int i = 1; i < n; i++) {
-			wt[i] = bt[i - 1] + wt[i - 1];
-		}
-		for (int i = 0; i < n; i++) {
-			tat[i] = bt[i] + wt[i];
-		}
-		double avgwt = 0;
-		for (int i : wt) {
-			avgwt += i;
-		}
-		avgwt /= n;
-		double avgtat = 0;
-		for (int i : tat) {
-			avgtat += i;
-		}
-		avgtat /= n;
-
-		System.out.println("Process No.\tBurst Time\tWaiting Time\tTurnAroundTime");
-		for (int i = 0; i < n; i++) {
-			System.out.println(pn[i] + "\t\t" + bt[i] + "\t\t" + wt[i] + "\t\t" + tat[i]);
-		}
-		System.out.println("Average Waiting Time: ");
-		System.out.println(avgwt);
-		System.out.println("Average TurnAround Time: ");
-		System.out.println(avgtat);
+		System.out.println("\nOutput of FCFS Scheduling: ");
+		print();
+		sc.close();
 	}
 }
--- CPU_Scheduling/RoundRobin.java
+++ CPU_Scheduling/RoundRobin.java
@@ -1,66 +1,93 @@
 import java.util.*;
+
+class Process {
+    int pid;
+    int bt;
+    int rem_bt;
+    int wt;
+    int tat;
+
+    Process(int pid, int bt) {
+        this.pid = pid;
+        this.bt = bt;
+        this.rem_bt = bt;
+    }
+
+}
+
 public class RoundRobin {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+
+    static Process proc[];
+    static int quantum;
+    static int p;
+    static double avgwt = 0;
+    static double avgtat = 0;
+
+    public static void input(Scanner sc) {
+
         System.out.println("Enter no of process");
-		int n = sc.nextInt();
+        p = sc.nextInt();
+        proc = new Process[p];
         System.out.println("Enter Time Quantam");
-        int quantum = sc.nextInt();
-        int[] jobs= new int[n];
-		int[] bursttime= new int[n];
-		for(int i=0;i<n;i++){
-			System.out.println("Enter Process number and Burst Time: ");
-			jobs[i] = sc.nextInt();
-			bursttime[i] = sc.nextInt();
-		}
-        int wt[] = new int[n];
-        int[] rem_bt = new int[n];
-        for(int i=0;i<n;i++){
-            rem_bt[i] = bursttime[i];
+        quantum = sc.nextInt();
+        for (int i = 0; i < p; i++) {
+            System.out.println("Enter Process number and Burst Time: ");
+            int pid = sc.nextInt();
+            int bt = sc.nextInt();
+            proc[i] = new Process(pid, bt);
         }
+    }
+
+    public static void roundrobin() {
         int t = 0;
-        while(true){
+        while (true) {
             boolean done = true;
-            for(int i=0;i<n;i++){
-                if(rem_bt[i]>0){
+            for (int i = 0; i < p; i++) {
+                if (proc[i].rem_bt > 0) {
                     done = false;
-                    if(rem_bt[i]>quantum){
-                        t+=quantum;
-                        rem_bt[i] -= quantum;
-                    }
-                    else{
-                        t+=rem_bt[i];
-                        wt[i] = t-bursttime[i];
-                        rem_bt[i] =0;
+                    if (proc[i].rem_bt > quantum) {
+                        t += quantum;
+                        proc[i].rem_bt -= quantum;
+                    } else {
+                        t += proc[i].rem_bt;
+                        proc[i].wt = t - proc[i].bt;
+                        proc[i].rem_bt = 0;
                     }
                 }
             }
-            if(done==true){
+            if (done == true) {
                 break;
             }
         }
-		
-        int[] tat = new int[n];
-        for(int i=0;i<n;i++){
-			tat[i] = wt[i]+ bursttime[i];
-		}
-        double avgwt=0;
-		for(int i:wt){
-			avgwt +=i;
-		}
-		avgwt/=n;
-		double avgtat=0;
-		for(int i:tat){
-			avgtat +=i;
-		}
-		avgtat/=n;
-		System.out.println("Process\t   BurstTime\t WaitingTime\t TurnAroundTime");
-		for(int i=0;i<n;i++){
-			System.out.println(jobs[i]+"\t\t"+bursttime[i]+"\t\t"+wt[i]+"\t\t"+tat[i]);
-		}
-		System.out.println("Average Waiting Time: "+avgwt);
-		System.out.println("Average Turn Around Time: "+avgtat);
-		sc.close();
+
+        for (int i = 0; i < p; i++) {
+            proc[i].tat = proc[i].wt + proc[i].bt;
+        }
+        for (Process i : proc) {
+            avgwt += i.wt;
+        }
+        avgwt /= p;
+        for (Process i : proc) {
+            avgtat += i.tat;
+        }
+        avgtat /= p;
+    }
+
+    public static void print() {
+        System.out.println("Process\tBurstTime\tWaitingTime\tTurnAroundTime");
+        for (int i = 0; i < p; i++) {
+            System.out.println(proc[i].pid + "\t\t" + proc[i].bt + "\t\t" + proc[i].wt + "\t\t" + proc[i].tat);
+        }
+        System.out.println("Average Waiting Time: " + avgwt);
+        System.out.println("Average Turn Around Time: " + avgtat);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        input(sc);
+        roundrobin();
+        System.out.println("\nOutput of RoundRobin(Premptive) Scheduling: ");
+        print();
+        sc.close();
     }
 }
-
