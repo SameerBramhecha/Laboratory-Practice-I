3bd53ee5b3aa48051684a79d00f43460236312c5
Sameer Manoj Bramhecha <bramhechas89@gmail.com>
2022-11-18 10:34:54 +0530

    Update SJF.java

--- CPU_Scheduling/SJF.java
+++ CPU_Scheduling/SJF.java
@@ -1,69 +1,118 @@
-import java.util.Scanner;
+import java.util.*;
+
+class Process {
+    int pid;
+    int at;
+    int bt;
+    int wt;
+    int rt;
+    int tat;
+
+    Process(int pid, int at, int bt) {
+        this.pid = pid;
+        this.at = at;
+        this.bt = bt;
+        this.rt = bt;
+    }
+
+}
 
 public class SJF {
+
+    static Process proc[];
+    static int p;
+    static double avgwt = 0;
+    static double avgtat = 0;
+
+    static void input(Scanner sc) {
+        System.out.println("Enter Number of Processes");
+        p = sc.nextInt();
+        proc = new Process[p];
+        for (int i = 0; i < p; i++) {
+            System.out.println("Enter ProcessNo., Arrival Time and Burst Time: ");
+            int pid = sc.nextInt();
+            int at = sc.nextInt();
+            int bt = sc.nextInt();
+            proc[i] = new Process(pid, at, bt);
+        }
+    }
+
+    static void sjf() {
+        int complete = 0, t = 0;
+        int minm = Integer.MAX_VALUE;
+        int shortest = 0, finish_time;
+        boolean check = false;
+
+        while (complete != p) {
+
+            for (int j = 0; j < p; j++) {
+                if ((proc[j].at <= t) &&
+                        (proc[j].rt < minm) && (proc[j].rt > 0)) {
+                    minm = proc[j].rt;
+                    shortest = j;
+                    check = true;
+                }
+            }
+
+            if (check == false) {
+                t++;
+                continue;
+            }
+
+            proc[shortest].rt--;
+
+            minm = proc[shortest].rt;
+
+            if (minm == 0) {
+                minm = Integer.MAX_VALUE;
+            }
+
+            if (proc[shortest].rt == 0) {
+                complete++;
+                check = false;
+
+                finish_time = t + 1;
+
+                proc[shortest].wt = finish_time - proc[shortest].bt - proc[shortest].at;
+
+                if (proc[shortest].wt < 0) {
+                    proc[shortest].wt = 0;
+                }
+            }
+            t++;
+
+        }
+
+        for (int i = 0; i < p; i++) {
+            proc[i].tat = proc[i].bt + proc[i].wt;
+        }
+
+        for (Process i : proc) {
+            avgwt += i.wt;
+        }
+        avgwt /= p;
+        for (Process i : proc) {
+            avgtat += i.tat;
+        }
+        avgtat /= p;
+    }
+
+    public static void print() {
+        System.out.println("ProcessNo.\tBurstTime\tArrivalTime\tWaitingTime\tTurnAroundTime");
+        for (Process i : proc) {
+            System.out.println(i.pid + "\t\t" + i.bt + "\t\t" + i.at + "\t\t" + i.wt + "\t\t" + i.tat);
+        }
+
+        System.out.println("Average Waiting Time: " + avgwt);
+        System.out.println("Average Turn Around Time: " + avgtat);
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter no of process");
-        int n = sc.nextInt();
-        int pid[] = new int[n];// process id
-        int at[] = new int[n];// arrival time
-        int bt[] = new int[n];// burst time
-        int ct[] = new int[n];// complete time
-        int ta[] = new int[n];// turnaround time
-        int wt[] = new int[n];// waiting time
-        int f[] = new int[n];// flag to check if process is completed or not
-        int k[] = new int[n];// it also stores burst time
-
-        int curtime = 0, tot = 0;
-        float avgwt = 0, avgta = 0;
-
-        for (int i = 0; i < n; i++) {
-            pid[i] = i + 1;
-            System.out.println("Enter Process " + (i + 1) + " arrival time: ");
-            at[i] = sc.nextInt();
-            System.out.println("Enter Process " + (i + 1) + " burst time: ");
-            bt[i] = sc.nextInt();
-            k[i] = bt[i];
-            f[i] = 0;
-        }
-        while (true) {
-            int min = Integer.MAX_VALUE;
-            int c = n;
-            if (tot == n) {
-                break;
-            }
-            for (int i = 0; i < n; i++) {
-                if ((at[i] <= curtime) && (f[i] == 0) && (bt[i] < min)) {
-                    min = bt[i];
-                    c = i;
-                }
-            }
-            if (c == n) {
-                curtime++;
-            } else {
-                bt[c]--;
-                curtime++;
-                if (bt[c] == 0) {
-                    ct[c] = curtime;
-                    f[c] = 1;
-                    tot++;
-                }
-            }
-        }
-        for (int i = 0; i < n; i++) {
-            ta[i] = ct[i] - at[i];
-            wt[i] = ta[i] - k[i];
-            avgwt += wt[i];
-            avgta += ta[i];
-        }
-
-        System.out.println("pid  arrival  burst  complete turn waiting");
-        for (int i = 0; i < n; i++) {
-            System.out.println(pid[i] + "\t" + at[i] + "\t" + k[i] + "\t" + ct[i] + "\t" + ta[i] + "\t" + wt[i]);
-        }
-
-        System.out.println("\naverage tat is " + (float) (avgta / n));
-        System.out.println("average wt is " + (float) (avgwt / n));
+        input(sc);
+        sjf();
+        System.out.println("\nOutput of FCFS Scheduling: ");
+        print();
         sc.close();
     }
 }
