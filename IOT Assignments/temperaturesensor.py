a0933f4081e3bebefd558fe7316758c38c22faa5
Sameer Manoj Bramhecha <bramhechas89@gmail.com>
2022-11-19 19:20:01 +0530

    Update temperaturesensor.py

--- IOT Assignments/temperaturesensor.py
+++ IOT Assignments/temperaturesensor.py
@@ -1,12 +1,21 @@
 import adafruit_dht
 import time
 import board
+import RPi.GPIO as GPIO
+
 dhtDevice = adafruit_dht.DHT22(board.D4)
+GPIO.setup(18,GPIO.IN) #Temparature Sensor
+GPIO.setup(4,GPIO.OUT) #LED out pin no
+
 while True:
     try:
         temperature = dhtDevice.temperature
         humidity = dhtDevice.humidity
         print('Temp={0:0.1f}*C Humidity={1:0.1f}%'.format(temperature,humidity))
+        if temperature > 25:
+            GPIO.output(4,True)
+            time.sleep(1.0)
+        GPIO.output(4,False)
     except RuntimeError as error:
         print(error.args[0])
         time.sleep(2.0)
@@ -14,4 +23,4 @@ while True:
     except Exception as error :
         dhtDevice.exit()
         raise error
-    time.sleep(2.0)
+    time.sleep(5.0)
